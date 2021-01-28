783a2785c53ba7a0a3079239ffeb4694df0c1043
pearlcrum <60251390+pearlcrum@users.noreply.github.com>
2021-01-28 22:15:58 +0900

    Add files via upload

---
+++ ImplicitConversion.java
@@ -0,0 +1,21 @@
+package variable;
+
+public class ImplicitConversion {
+
+	public static void main(String[] args) {
+		
+		byte bNum =10;
+		int iNum= bNum;
+		
+		System.out.println(bNum);
+		System.out.println(iNum);
+		
+		int iNum2=20;
+		float fNum= iNum;
+		
+		
+		
+		System.out.println(fNum); 
+	}
+
+}
---
+++ IntegerTest.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class IntegerTest {
+
+	public static void main(String[] args) {
+		
+		byte bs = -128;
+		System.out.println(bs);
+
+	}
+
+}
